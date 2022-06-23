e163400947dcbb781f0b3c77367aa6c39df8fe27
cater20 <onyangoketta@gmail.com>
2022-06-23 23:43:06 +0300

    package.json

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 // event listeners
 
-document.querySelector('#fruit-form').addEventListener('submit', e)
+//document.querySelector('#fruit-form').addEventListener('submit', e)
 
 
 //eventhandlers
-function handleSubmit(e) {
-    e.preventDefault()
+//function handleSubmit(e) {
+    //e.preventDefault()
 
-    let fruitObj={
-        name:e.target.fruit.value,
-        name:e.target.image_url.value,
-        description:e.target.description.value,
-        buyFruit:0
-    } 
-} 
-renderOneFruit(fruitObj)
-buyFruit(fruitObj)
+   // let fruitObj={
+     //   name:e.target.fruit.value,
+      //  name:e.target.image_url.value,
+     //   description:e.target.description.value,
+   //     buyFruit:0
+  //  } 
+//} 
+//renderOneFruit(fruitObj)
+//buyFruit(fruitObj)
 
 //dom
 function renderOneFruit(animal) {
     let card=document.createElement('li')
-    card.className="card"
-    card.id=fruit.id
-    card.innerHTML=`
+    card.className ="form"
+    card.id = fruit.id
+    card.innerHTML =`
     
 < div class ="content">
 <h2>${fruit.name}</h2>
@@ -40,17 +40,17 @@ ${fruit.description}</p>
 </div>
     `
 
-    card.querySelector('#payment').addEventListener('click',()=>{
-        fruit.payment +=10
-        card.querySelector('span').textContent=fruit.payment
-        updatePayment(fruit)
-    })
-    card.querySelector('#set_pass').addEventListener('click',()=>{
-    card.remove()
-    deletefruit(fruit.id)
-})
+    //card.querySelector('#payment').addEventListener('click',()=>{
+      //  fruit.payment +=10
+       // card.querySelector('span').textContent=fruit.payment
+     //   updatePayment(fruit)
+   // })
+  //  card.querySelector('#set_pass').addEventListener('click',()=>{
+   // card.remove()
+   // deletefruit(fruit.id)
+//})
 }
-   document.querySelector('#fruit-list').appendChild(card) 
+  // document.querySelector('#fruit-list').appendChild(form) 
     
 
 
@@ -58,45 +58,47 @@ ${fruit.description}</p>
 function getAllFruits() {
     fetch("http://localhost:3000/fruits")
     .then (res =>res.json())
-    .then (fruitData =>fruitData.forEach(fruit => renderOneFruit(fruit))
+   // .then (fruit =>fruit.forEach(fruit => renderOneFruit(fruit))
         
-    );
+    //);
 }
-function buyFruit(fruitObj) {
-    fetch ("http://localhost:3000/fruits",{
-        method:"POST",
-        headers:{
-            "content.Type":"application/json"
-        },
-       body:JSON.stringify(fruitObj) 
-    } )
-    .then (res => res.json())
-    .then(fruit => console.log(fruit))
-}
-function updatePayment(fruitObj) {
-    fetch (`http://localhost:3000/fruits/${fruitObj.id}`,{
-        method:"PATCH",
-        headers:{
-            "content.Type":"application/json"
-        },
-       body:JSON.stringify(fruitObj) 
-    } )
-    .then (res => res.json())
-    .then(fruit => console.log(fruit))
-}
-function deleteFruit(id) {
-    fetch (`http://localhost:3000/fruits/${id}`,{
-        method:"DELETE",
-        headers:{
-            "content.Type":"application/json"
-        },
+//function buyFruit(fruitObj) {
+  //  fetch ("http://localhost:3000/fruits",{
+      //  method:"POST",
+      //  headers:{
+       //     "content.Type":"application/json"
+       // },
+      // body:JSON.stringify(fruitObj) 
+   // } )
+  //  .then (res => res.json())
+  //  .then(fruit => console.log(fruit))
+//}
+//function updatePayment(fruitObj) {
+   // fetch (`http://localhost:3000/fruits/${fruitObj.id}`,{
+    //    method:"PATCH",
+      //  headers:{
+        //    "content.Type":"application/json"
+        //},
+      // body:JSON.stringify(fruitObj) 
+  //  } )
+   // .then (res => res.json())
+   // .then(fruit => console.log(fruit))
+//}
+//function deleteFruit(id) {
+   // fetch (`http://localhost:3000/fruits/${id}`,{
+     //   method:"DELETE",
+    //    headers:{
+         //   "content.Type":"application/json"
+     //   },
     
-    } )
-    .then (res => res.json())
-    .then(fruit => console.log(fruit))  
-}
+   // } )
+   // .then (res => res.json())
+  //  .then(fruit => console.log(fruit))  
+//}
 
 function initialize() {
+    //fruits.forEach(fruit=>renderOneFruit(fruit))
     getAllFruits()
 }
+console.log(getAllFruits);
 initialize
